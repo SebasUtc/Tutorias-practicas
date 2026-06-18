f0e2c7a3e88fe5b25dfa3bf9c53d20d45b23b67e
SebasUtc <110674261+SebasUtc@users.noreply.github.com>
2026-06-18 10:44:00 -0500

    preparación de urls y views

--- Inicio/views.py
+++ Inicio/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login as auth_login
+def hola(request):
 # Create your views here.
+   return render(request, 'index.html')
+def contact(request):
+   return render(request, 'header.html')
+def courses(request):
+   return render(request, 'footer.html')
