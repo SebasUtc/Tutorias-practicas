4a144cb970ab0cdddb55e2cefa255fa6a4cf008e
SebasUtc <110674261+SebasUtc@users.noreply.github.com>
2026-06-21 17:08:20 -0500

    added single html

--- Inicio/urls.py
+++ Inicio/urls.py
@@ -3,5 +3,13 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('', views.hola, name='home'),
-    
+    path('header/', views.header, name='header'),
+    path('footer/', views.footer, name='footer'),
+    path('shop/', views.shop, name='shop'),
+    path('contact/', views.contact, name='contact'),
+    path('cheackout/', views.cheackout, name='cheackout'),
+    path('bestsellers/', views.bestsellers, name='bestsellers'),
+    path('single/', views.single, name='single'),
+    path('cart/', views.cart, name='cart'),
+    path('error/', views.error, name='error'),
 ]
--- Inicio/views.py
+++ Inicio/views.py
@@ -9,3 +9,17 @@ def header(request):
    return render(request, 'header.html')
 def footer(request):
    return render(request, 'footer.html')
+def shop (request):
+   return render(request, 'shop.html')
+def contact(request):
+   return render(request, 'contact.html')
+def cheackout(request):
+   return render(request, 'cheackout.html')
+def bestsellers(request):
+   return render(request, 'bestsellers.html')
+def single(request):
+   return render(request, 'single.html')
+def cart(request):
+   return render(request, 'cart.html')
+def error(request):
+   return render(request, 'error.html')
